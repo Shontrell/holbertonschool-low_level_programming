f4a1a81b33a384472b0956381ad443d6377738a7
shontrell <shontrell.scott3@gmail.com>
2020-02-07 04:04:55 +0000

    updating 0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
-#include <string.h>
- 
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
-
 {
-  char string[50];
-
- strcpy(string, "\"Programming is like building a multilingual puzzle");
-
- puts(string);
-
- return (0);
+char string[50];
+strcpy(string, "\"Programming is like building a multilingual puzzle");
+puts(string);
+return (0);
 }
