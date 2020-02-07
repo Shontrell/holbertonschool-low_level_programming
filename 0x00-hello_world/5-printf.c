14af573d346ba6118eb23052ab492650b3c4ac06
shontrell <shontrell.scott3@gmail.com>
2020-02-07 05:02:16 +0000

    updating 0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-  // displays the string inside quotations
-  printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+char str [60] = ("with proper grammar, but the outcome is a piece of art,");
+ printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
