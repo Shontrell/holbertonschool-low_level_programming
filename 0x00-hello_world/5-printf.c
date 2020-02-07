6a5a01451e37a197cff113056516cebde88871a0
shontrell <shontrell.scott3@gmail.com>
2020-02-07 04:52:33 +0000

    updating 0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 int main(void)
 {
   // displays the string inside quotations
-printf("with proper grammar, but the outcome is a piece of art,");
+  printf("%s\n","with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
