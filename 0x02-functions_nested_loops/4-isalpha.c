2ed996334b76272037111e93ee5826b65619eff2
shontrell <shontrell.scott3@gmail.com>
2020-02-11 20:20:59 +0000

    updating 0x02-function_nested_loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isalpha - Entry point
+ *
+ * @c: int passed as argument to the function
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if(c >= 65 && c <= 122)
+	{
+		return (1)
+			}
+	else
+	{ return (0)
+			}
+}
