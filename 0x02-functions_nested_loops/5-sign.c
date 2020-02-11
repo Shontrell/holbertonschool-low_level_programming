c7834d2367b4eb442fa26784c14f1c8d682594db
shontrell <shontrell.scott3@gmail.com>
2020-02-11 20:53:19 +0000

    updating 0x02-function_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_sign - Entry point
+ *
+ * @n: int passed as argument to the function
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{ _putchar (43);
+		return (1);
+			}
+	else if ( n == 0)
+	{ _putchar ('0');
+		return (0);
+	}
+	else
+	{ _putchar (45);
+		return (-1);
+	}
+}
