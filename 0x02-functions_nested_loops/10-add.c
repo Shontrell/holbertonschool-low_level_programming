986837470cb876a7e0efd8acbac1ac59ef66d83d
shontrell <shontrell.scott3@gmail.com>
2020-02-12 04:15:16 +0000

    updating 0x02-function_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * add - Entry point
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
