317773a36517e26554137f1fae3f3915fc99b55a
shontrell <shontrell.scott3@gmail.com>
2020-02-14 00:24:45 +0000

    updating 0x04 function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,4 +6,26 @@
  */
 void more_numbers(void)
 {
-int x
+	int y;
+	int x;
+	int z;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		for (x = '0'; x <= '1'; x++)
+		{
+			for (z = '0'; z <= '9'; z++)
+			{
+				if (x == '1' && z <= '4')
+				{
+					_putchar(x);
+				}
+				if (!(x == '1' && z > '4'))
+				{
+					_putchar(z);
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
