b4d1ab289007426aed65d608e83b0291b1e036ce
shontrell <shontrell.scott3@gmail.com>
2020-02-18 15:51:35 +0000

    updating 0x05 pointers arrays strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,15 @@
 #include "holberton.c"
+/**
+ * _strlen = returns the length of a string
+ *
+ * @s: string being measured for length
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *(s + len) != '/0'; len++)
+		return (len)
+			}
