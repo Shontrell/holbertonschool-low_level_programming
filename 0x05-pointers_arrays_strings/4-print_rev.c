f38529e5ddf83822adf8c7f1233599c7dc0a9ecc
shontrell <shontrell.scott3@gmail.com>
2020-02-18 20:25:44 +0000

    updating 0x05 pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * @s: string getting printed in reverse
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int str = 0;
+
+	while (s[str] != '\0')
+	{
+		str++;
+	}
+	while (str >= 0)
+	{
+		_putchar(s[str]);
+			str--;
+	}
+	_putchar('\n');
+}
