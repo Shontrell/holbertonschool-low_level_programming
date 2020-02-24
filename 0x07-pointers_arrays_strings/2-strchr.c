e64d74518eb8f9e7335fe8531046685c9936a92e
shontrell <shontrell.scott3@gmail.com>
2020-02-24 20:48:57 +0000

    updating 0x07 pointers arrays strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,16 @@
 #include "holberton.h"
+/**
+ * *_strchr - locates a character in a string
+ * @s: pointer
+ * @c: character
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (0);
+}
