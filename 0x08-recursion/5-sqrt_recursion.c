9e863dd3a33f1c6c3e64f9b9949660348be1c01b
shontrell <shontrell.scott3@gmail.com>
2020-02-27 04:23:54 +0000

    updating 0x08

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * findsr - find the square root of a number
+ * @sr: int
+ * @s: int
+ * Return: 0
+ */
+int findsr(int sr, int s)
+{
+	if ((s * s) == sr)
+	{
+		return (s);
+	}
+	if ((s * s) > sr)
+	{
+		return (-1);
+	}
+	return (findsr(sr, s + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: int
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (findsr(n, 0));
+}
