0c52f041ddea33c3d0be816e2c126c8948c42e54
shontrell <shontrell.scott3@gmail.com>
2020-03-01 22:58:44 +0000

    updating 0x0A argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: int
+ * @argv: pointer
+ * Return: 0
+ */
+int atoi(__attribute__((unused)) char *argv);
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * (atoi(argv[i]));
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
