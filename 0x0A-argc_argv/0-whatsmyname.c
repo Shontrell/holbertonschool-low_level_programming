fac24d108198e1d574d1cfd6da4815d04b4dc914
shontrell <shontrell.scott3@gmail.com>
2020-03-01 21:00:02 +0000

    updating 0x0A argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * @argc - int
- * @argv - pointer
+ * @argc: int
+ * @argv: pointer
  * Return: 0
  */
 int main(__attribute__((unused)) int argc, char **argv)
