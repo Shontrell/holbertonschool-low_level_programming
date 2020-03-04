afbf70c018f51becb6aabe0cc3e5d99607459884
shontrell <shontrell.scott3@gmail.com>
2020-03-04 23:02:30 +0000

    updating 0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,5 +8,35 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int row, col;
+	int **p;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	if (p)
+	{
+		p = malloc(sizeof(int*) * height);
+		for (row = 0; row < height; row++)
+		{
+			if (p[row])
+			{
+				p[row] = malloc(sizeof(int) * width);
+				for (col = 0; col < width; col++)
+				{
+					p[row][col] = 0;
+				}
+			}
+			else
+			{
+				return (NULL);
+			}
+		}
+		return (p);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
