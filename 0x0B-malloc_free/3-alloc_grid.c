1d1d2ebe95e34007deb0e095608f92239db463fc
shontrell <shontrell.scott3@gmail.com>
2020-03-04 02:02:20 +0000

    updating 0x0B-malloc_free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * **alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: int
+ * @height: int
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+
+}
