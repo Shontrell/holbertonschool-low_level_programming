5baaa9fcdbe5dbb78393c709539e6d7728cdf8dc
shontrell <shontrell.scott3@gmail.com>
2020-03-05 18:07:58 +0000

    updating 0x0C- more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = (int*)malloc(sizeof(int) * b);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	exit (98);
+}
