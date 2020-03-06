8bc7c85c576bb6642f3cf6e9d4a40c81e5afe3ff
shontrell <shontrell.scott3@gmail.com>
2020-03-06 01:35:04 +0000

    updating 0x0C- more malloc and free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,8 +11,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 	unsigned int count;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0)
 		return (NULL);
+	if (size == 0)
+		return (NULL);
+
 	p = (void *) malloc(nmemb * size);
 	if (p == NULL)
 	{
