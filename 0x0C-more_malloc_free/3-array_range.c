6f97146ccfeeadb9a6b72038f6cd21704d3d8f80
shontrell <shontrell.scott3@gmail.com>
2020-03-06 03:08:16 +0000

    updating 0x0C- more malloc and free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,11 +11,17 @@ int *array_range(int min, int max)
 	int *p, count;
 
 	if (min > max)
+	{
 		return (NULL);
+	}
 	p = malloc(sizeof(int) * (max - min) + 1);
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 	for (count = 0; count <= (max - min); count++)
+	{
 		p[count] = min + count;
+	}
 	return (p);
 }
