2c64216893e4d10a64a7abedc7c131f3b6545ee9
shontrell <shontrell.scott3@gmail.com>
2020-03-09 22:03:18 +0000

    updating 0x0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - name, owner, and age
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ * Description: dog's details from name, owner, and age
+ */
 struct dog
 {
 	char *name;
