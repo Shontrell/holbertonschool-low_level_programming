1169090a63fc69fe2b08faf6f53da0289a204e20
shontrell <shontrell.scott3@gmail.com>
2020-03-10 02:40:56 +0000

    updating 0x0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: dog's age
  * Description: dog's details from name, owner, and age
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
