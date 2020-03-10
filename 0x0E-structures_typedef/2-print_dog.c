db91815f81a6b82b29a1ac04c2afc363d076dc0c
shontrell <shontrell.scott3@gmail.com>
2020-03-10 03:55:08 +0000

    updating 0x0E

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,12 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (d->name == 0)
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	if (d->owner == 0)
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
