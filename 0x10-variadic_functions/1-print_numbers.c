f0cd90629c56e44c47d2ee47ddb12e690109ec90
shontrell <shontrell.scott3@gmail.com>
2020-03-14 19:10:11 +0000

    updating

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: pointer to char
+ * @n: unsigned int
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int x, numb = 0;
+
+	va_start(ap, n);
+
+	if (separator == NULL)
+	{
+		va_end(ap);
+		return;
+	}
+	if (n == 0)
+	{
+		va_end(ap);
+		return;
+	}
+	for (x = 0; x < n; x++)
+	{
+		numb = va_arg(ap, int);
+		if (x == (n - 1))
+		{
+			printf("%d", numb);
+		}
+		else
+		{
+		printf("%d%s ", numb, separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: pointer to char
+ * @n: unsigned int
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
