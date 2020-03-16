96f4f42eae9b8ae16aba9a305850fb3b4e7a3e3f
shontrell <shontrell.scott3@gmail.com>
2020-03-16 00:03:52 +0000

    first draft of code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,5 +8,30 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+        va_list ap;
+        unsigned int x;
+        int *str;
 
+        va_start(ap, n);
+        for (x = 0; x < n; x++)
+        {
+                str = va_arg(ap, int);
+		if (str == NULL)
+			printf("nil");
+                if (x == (n - 1))
+                        printf("%s", str);
+                else
+                {
+                        if (separator != NULL)
+                        {
+				printf("%s%s", str, separator);
+                        }
+                        else
+                        {
+                                printf("%s", str);
+                        }
+                }
+        }
+        printf("\n");
+        va_end(ap);
 }
