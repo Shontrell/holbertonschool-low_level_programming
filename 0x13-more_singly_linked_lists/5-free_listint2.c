e6f74698fd705c56f76814543807534154401e6c
shontrell <shontrell.scott3@gmail.com>
2020-03-24 15:59:34 +0000

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to listint_t list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *hold;
+
+	while (head != NULL)
+	{
+		hold = *head;
+		*head = (*head)->next;
+		free(hold);
+	}
+}
