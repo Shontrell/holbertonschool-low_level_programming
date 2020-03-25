3c0db4174b815f0a325c548196498d0910d28c0e
shontrell <shontrell.scott3@gmail.com>
2020-03-25 02:54:49 +0000

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
  * *insert_nodeint_at_index - inserts a new node at a given position
- * @head:
- * @idx:
- * @n:
+ * @head: pointer to pointer to listint_t
+ * @idx: index of the list where new node should be added
+ * @n: value of node
  * Return: address of the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (insert == NULL)
 	{
 		free(insert);
-		return(NULL);
+		return (NULL);
 	}
 	if (idx == 1)
 	{
