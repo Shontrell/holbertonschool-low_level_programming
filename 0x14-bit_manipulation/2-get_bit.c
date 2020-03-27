ccb1366556452c56e925b99ff8ed5cd94e0ae62e
shontrell <shontrell.scott3@gmail.com>
2020-03-27 01:58:41 +0000

    returns the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: int
+ * @index: the index starting from 0
+ * Return: the value of the bit at the given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	int bit;
+
+	for (i = 0; i < index; i++)
+	{
+		n = n >> 1;
+	}
+	bit = n & 1;
+	return (bit);
+}
