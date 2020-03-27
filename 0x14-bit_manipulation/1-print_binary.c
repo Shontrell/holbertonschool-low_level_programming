13d03eddcb082d2d9ab5327e8371085d21ed84db
shontrell <shontrell.scott3@gmail.com>
2020-03-27 00:30:52 +0000

    prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: int to be converted to binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int x = n & 1;
+
+	if (n > 1)
+		print_binary(n>>1);
+	_putchar(x + '0');
+}
